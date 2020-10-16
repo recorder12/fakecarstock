17b8914e104f01c84371d663d7afb7a40aadd654
recorder12 <byeonggeon@naver.com>
2020-10-15 23:39:52 -1000

    change description

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,6 +4,6 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "fakecarstock";
   res.locals.routes = routes;
   res.locals.description =
-    "'FakeCarStock' 은 넷상에 존재하는 유사 차량 이미지를 검색하여 제공하는 사이트입니다. 실매물 검색은 제공하지 않으며 이를 위해 하단에 링크된 실매물 검색 사이트 사용을 추천드립니다. ";
+    "'FakeCarStock.com'은 업로드한 차량 이미지와 동일한 차량 매물을 대형 중고차 사이트에서 검색하여 보여드립니다. 검색된 매물이 존재한다면 확인 후 조심히 거래하시길 권장드립니다.";
   next();
 };
